2202ac7cd56a96b9f14c72e26068d6c3d2b7f803
Lopes <evandrolopes7@gmail.com>
2019-02-23 16:39:43 -0300

    Commit 2

---
+++ Teste_RWC_Arquivos.py
@@ -0,0 +1,22 @@
+teste2 = open('testeExcel.xls','w')
+teste3 = open('testeWord.docx','w')
+teste4 = open('testeCSV.csv','w')
+teste5 = open('testePython.py','w')
+
+docx = input()
+csv = input()
+
+teste = open('testeTxt.html', 'w')
+teste.write(docx)
+teste.close()
+
+teste2.write(csv)
+teste3.write(input())
+teste4.write(input())
+teste5.write(input())
+
+
+teste2.close()
+teste3.close()
+teste4.close()
+teste5.close()
