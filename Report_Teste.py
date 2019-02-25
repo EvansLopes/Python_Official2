4de8247089d0ea963a99766cd0b07838cd38e500
Lopes <evandrolopes7@gmail.com>
2019-02-25 19:50:00 -0300

    Voltando aos testes com PDF no Python Beck in teste with pdf file format in python.

--- Report_Teste.py
+++ Report_Teste.py
@@ -1,9 +1,12 @@
 from reportlab.pdfgen import canvas
 
-texto1 = input()
+headerText = input()
+subHeaderText = input()
 def main(args):
     cnv = canvas.Canvas('Alo_Mundo.pdf')
-    cnv.drawString(150,450,texto1)
+    cnv.drawString(1,500,headerText)
+    cnv.drawString(20,12,subHeaderText)
+
     cnv.save()
     return 0
 if __name__ == '__main__':
