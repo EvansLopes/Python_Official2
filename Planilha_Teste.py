1853bef7f9e24ea8a89b8813225ace3d53a04261
Lopes <evandrolopes7@gmail.com>
2019-02-25 19:41:33 -0300

    Projeto de leitura de Excel com python Contém erros, mas estou analisando o breve código.

--- Planilha_Teste.py
+++ Planilha_Teste.py
@@ -1,3 +1,11 @@
-import pandas as pd
-import numpy as np
+import openpyxl as ex
 
+wb = ex.load_workbook('Primeira_Planilha.xls')
+
+folhas = wb.get_sheet_names()
+sheet = wb.get_sheet_names('Pasta1')
+
+nr = sheet.get_highest_row()
+nc = sheet.get_highest_column()
+print('rows = ', nr)
+print('columns = ',nc)
