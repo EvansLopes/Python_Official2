3ed8701d4bed9cca09aec59cd3db01be13be4b93
Lopes <evandrolopes7@gmail.com>
2019-02-28 10:44:34 -0300

    28/02/2019 Melhorias implantadas: -Mapeamento de tamanho da página; -Inserção de Nomes de Colunas; -Centralização do título e do subtítulo

--- Report_Teste.py
+++ Report_Teste.py
@@ -2,12 +2,12 @@ from reportlab.pdfgen import canvas
 
 headerText = input().center(0)
 subHeaderText = input().center(0)
-print(164-(len(headerText)/2))
+print(164+(len(headerText)/2))
 
 def main(args):
     cnv = canvas.Canvas('Relatorio de texto.pdf')
-    cnv.drawString(164-(len(headerText)/2), 800, 'Título: ' + headerText)
-    cnv.drawString(164-(len(subHeaderText)/2), 788, 'Nome: ' + subHeaderText)
+    cnv.drawString(164+(len(headerText)/2), 800, 'Título: ' + headerText)
+    cnv.drawString(164+(len(subHeaderText)/2), 788, 'Nome: ' + subHeaderText)
 #-(len(headerText)/2)
     cnv.drawString(30, 770, 'Nome do funcionário')
     cnv.drawString(230, 770, 'Idade do funcionário')
